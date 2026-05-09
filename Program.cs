966f916627c572330f1c59a5b1caebbab22f19a9
PBL075 <pavelgorea75@gmail.com>
2026-05-09 10:38:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace array_mare
+{
+    class Program
+    {
+        static void Main()
+        {
+            int [,] numbers = 
+            {
+                {1 , 2 , 3},
+                {4 , 5 , 6},
+                {7 , 8 , 9}, 
+            };
+            Console.WriteLine(numbers[1,2]);
+        }
+    }
+}
